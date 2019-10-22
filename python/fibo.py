b4928c1f6926538059770e11093ee38fd157c3da
Aiden Park <pyj789123@naver.com>
2019-10-22 15:07:10 +0900

    add fibo func

---
+++ python/fibo.py
@@ -0,0 +1,23 @@
+# recursive function
+def fibo(num):
+    if num <= 1:
+        return num
+    else:
+        return fibo(num - 1) + fibo(num - 2)
+
+# while loop
+
+def fiboLoop(num):
+    prev = 1
+    prev_prev = 0
+    curr = 0
+    
+    while num != 1:
+        num -= 1
+        curr = prev + prev_prev
+        prev_prev = prev
+        prev = curr
+    
+    return curr
+        
+print(fiboLoop(5))
